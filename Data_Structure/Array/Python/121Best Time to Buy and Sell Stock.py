7a6704b8b77d3ac714eb0b491800f72890954626
YuweiSophieWang <60522232+Yuweien@users.noreply.github.com>
2022-05-23 15:47:04 -0700

    121

---
+++ Data_Structure/Array/Python/121Best Time to Buy and Sell Stock.py
@@ -0,0 +1,23 @@
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit = 0.
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            if price - min_price > max_profit:
+                max_profit = price - min_price
+        return max_profit
+
+res = Solution()
+print(res.maxProfit([7,1,5,3,6,4]))
