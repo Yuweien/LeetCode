d0d9e61de77c8bb2f3cc9a3d73e7707aa23deedf
YuweiSophieWang <60522232+Yuweien@users.noreply.github.com>
2022-05-30 17:52:33 -0700

    Linked list, two pointers

---
+++ Algorithm/Python/876. Middle of the Linked List.py
@@ -0,0 +1,17 @@
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional, ListNode
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
